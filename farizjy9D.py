68d1ba5980901edb58b4d1a0542d00d32d630d68
tiksmpn4depok <tiksmpn4depok@gmail.com>
2026-05-20 15:12:49 +0700

    Add files via upload

---
+++ farizjy9D.py
@@ -0,0 +1,25 @@
+# untuk fano
+
+print('balkadag')
+print(37)
+
+# operator aritmatika
+print(1+2)
+print(1-2)
+print(1*2)
+print(1/2)
+print(2**3)
+print(37//5)
+print(10%5)
+nama = 'refano'
+nama = 'kenzi'
+print(nama)
+Nama ='refano'
+print(Nama)
+angka1 = 10
+angka2 = 11
+print(angka1+angka2)
+nama_depan = 'refano'
+nama_akhir = 'praba'
+print(nama_depan+' '+nama_akhir)
+print(nama_depan,nama_akhir)
